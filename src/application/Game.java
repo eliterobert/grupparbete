bb84eea1845cf569ac0683d700df0a01b31f5ee0
Alexander Walfridsson <alexander.walfridsson@live.com>
2015-10-26 14:41:02 +0100

    Lade till klassen turn och text.

--- src/application/Game.java
+++ src/application/Game.java
@@ -2,4 +2,17 @@ package application;
 
 public class Game {
 
+	/*
+	 *Metoder:
+	 *Time lapsed: ränkar hur länge spelet pågått,
+	 *Score: lägger till poäng till den aktiva spelaren,
+	 *Player Turn: metod som bestämmer vilken spelares tur att spelare det är. 
+	 *Which turn: räknar ut vilken total runda det är.
+	 *
+	 *instansvariabler:
+	 *Score: poängen som skickas till spelaren,
+	 *Time: en nummervariabel
+	 */
+	
+	
 }
