462362c6f0bf2412ae0662b8e1faa127d4228dac
Alexander Walfridsson <alexander.walfridsson@live.com>
2015-10-26 10:56:30 +0100

    Added System out print line.

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		
+			System.out.println("Nytillagd System out print");
 	}
 	
 	public static void main(String[] args) {
