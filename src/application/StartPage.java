3ebf115efbb2d23f76d3e261dba6a7652126eea4
selhadin <tittobozan@gmail.com>
2015-10-26 15:45:53 +0100

    Changed Main

--- src/application/StartPage.java
+++ src/application/StartPage.java
@@ -1,25 +1,37 @@
 package application;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
-import javafx.stage.Stage;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-public class StartPage  {
+public class StartPage extends VBox {
 
-	public void start(Stage primaryStage) {
-		FlowPane root = new FlowPane();
-		Scene scene = new Scene(root,400,400);
-		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-		Label topLabel=new Label("Memory Game");
-		
-		
-		
+	Label topLabel;
+	Label antalSpelare;
+	Button startButton;
+	ToggleGroup tg;
+	RadioButton r1, r2, r3;
+	Spinner spin;
+	HBox hbox;
+
+	//		Label topLabel=new Label("Memory Game");
+	//		Label antalSpelare = new Label("Antal Spelare");
+
+	public StartPage(){
+		topLabel = new Label("Memory Game");
+		antalSpelare= new Label("Antal Spelare");
+		startButton=new Button("Start");
+
+		hbox=new HBox(hbox);
 	}
+
+
+
+
 
 
 }
