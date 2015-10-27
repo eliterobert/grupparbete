d617d6ea5cc50946c3f3fc95a423009d551fbb3d
RobertE <roberteriksson_@hotmail.com>
2015-10-27 14:38:40 +0100

    Added a method to read pictures

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,30 +10,35 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		
-			
-	}
-	
-	public void picsFromFile()
-	{
-		File f = new File("Pictures\\");
-		ArrayList<File> imageList = new ArrayList<>(Arrays.asList(f.listFiles()));
-		
-		
-		
-	}
-	
+
+	private ArrayList<Image> imageList;
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	@Override
+	public void start(Stage primaryStage) {
+
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root, 400, 400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+
+	private void getPictures() {
+		imageList = new ArrayList<>();
+
+		File imageDirectory = new File("src/Pictures");
+		String[] directoryList = imageDirectory.list();
+
+		for (int i = 0; i < directoryList.length; i++) {
+			imageList.add(new Image("Pictures/" + directoryList[i]));
+		}
+
+	}
+
 }
