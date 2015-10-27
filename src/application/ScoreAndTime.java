473d9490578c560f9ed43f6c94a0c5f5defa7c5d
jonthoe <jonathannore@gmail.com>
2015-10-27 10:53:06 +0100

    Instansvariabel, constructor and some getters and setters

--- src/application/ScoreAndTime.java
+++ src/application/ScoreAndTime.java
@@ -1,18 +1,41 @@
 package application;
 
+/*
+ *Metoder:
+ *Time lapsed: ränkar hur länge spelet pågått,
+ *Score: lägger till poäng till den aktiva spelaren,
+ *
+ *instansvariabler:
+ *Score: poängen som skickas till spelaren,
+ *Time: en nummervariabel
+ *
+ */
+
+
 public class ScoreAndTime {
 
-	/*
-	 *Metoder:
-	 *Time lapsed: ränkar hur länge spelet pågått,
-	 *Score: lägger till poäng till den aktiva spelaren,
-	 *
-	 *instansvariabler:
-	 *Score: poängen som skickas till spelaren,
-	 *Time: en nummervariabel
-	 */
+		int score;
+		
 	
+		
+		// Constructor 
+
+	public ScoreAndTime(int score) {
+			super();
+			this.score = score;
+	}
+		
+		// Getters och setters 
+
+	public int getScore() {
+			return score;
+	}
+
+	public void setScore(int score) {
+			this.score = score;
+	}
 	
-	
-	
+			
+			
+		
 }
