5a951f34df4ff05eeb6a5acf2b18547334eea98c
Bahawodin <bahawodin@gmail.com>
2015-10-27 10:36:20 +0100

    Expanded the constructor of Card

--- src/application/Card.java
+++ src/application/Card.java
@@ -1,13 +1,26 @@
 package application;
 
+import javafx.geometry.Pos;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.transform.Rotate;
 
 public class Card extends StackPane{
 		ImageView faceView, backView;
 		
 		//Konstruktor för Card
-		public Card() {
+		public Card(Image faceImg, Image backImg) {
+			faceView = new ImageView(faceImg);
+			backView = new ImageView(backImg);
+			faceView.setRotationAxis(Rotate.Y_AXIS);
+			faceView.setRotate(180);
+			faceView.setOpacity(0);
+			
+
+			setAlignment(Pos.CENTER);
+			setRotationAxis(Rotate.Y_AXIS);
+			getChildren().addAll(faceView, backView);
 			
 		}
 		
