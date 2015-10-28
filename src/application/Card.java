d30db9e6d35d262be505b12b8c6025321f0019c9
Bahawodin <bahawodin@gmail.com>
2015-10-28 10:25:12 +0100

    Changed the open and close methods

--- src/application/Card.java
+++ src/application/Card.java
@@ -10,66 +10,58 @@ import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
 
 public class Card extends StackPane{
-		ImageView faceView, backView;
-		
-		//Constructor for Card
-		public Card(Image faceImg, Image backImg) {
-			faceView = new ImageView(faceImg);
-			backView = new ImageView(backImg);
-			faceView.setRotationAxis(Rotate.Y_AXIS);
-			faceView.setRotate(180);
-			faceView.setOpacity(0);
-			
 
-			setAlignment(Pos.CENTER);
-			setRotationAxis(Rotate.Y_AXIS);
-			getChildren().addAll(faceView, backView);
-			
-//			setOnMouseClicked(value);
-			
-		}
-		
-		public boolean isOpen() {
-			return getRotate() > 0;
-			
-		}
-		
-		public boolean isOfSameKind(Card otherCard) {
-			return this.faceView.getImage().equals(otherCard.faceView.getImage());
-			
-		}
-		
-		public void open(Runnable action) {
-			FadeTransition faceOpacity = new FadeTransition(Duration.millis(1), faceView);
-			faceOpacity.setToValue(1);
-			faceOpacity.setDelay(Duration.millis(500));
-			faceOpacity.play();
+	RotateTransition rotation;
+	ImageView faceView, backView;
 
-			FadeTransition backOpacity = new FadeTransition(Duration.millis(1), backView);
-			backOpacity.setToValue(0);
-			backOpacity.setDelay(Duration.millis(500));
-			backOpacity.play();
+	//Constructor for Card
+	public Card(Image faceImg, Image backImg) {
+		faceView = new ImageView(faceImg);
+		backView = new ImageView(backImg);
 
-			RotateTransition rotateForward = new RotateTransition(Duration.millis(1000), this);
-			rotateForward.setToAngle(180);
-			rotateForward.play();
+		setAlignment(Pos.CENTER);
+		setRotationAxis(Rotate.Y_AXIS);
+		getChildren().addAll(faceView, backView);
 
-			rotateForward.setOnFinished((a) -> action.run());
-		}
-		
-		public void close() {
-			FadeTransition faceOpacity = new FadeTransition(Duration.millis(1), faceView);
-			faceOpacity.setToValue(0);
-			faceOpacity.setDelay(Duration.millis(1000));
-			faceOpacity.play();
+		setOnMouseClicked((ae) -> open());
 
-			FadeTransition backOpacity = new FadeTransition(Duration.millis(1), backView);
-			backOpacity.setToValue(1);
-			backOpacity.setDelay(Duration.millis(1000));
-			backOpacity.play();
+	}
+	
+	
 
-			RotateTransition rotateBackward = new RotateTransition(Duration.millis(2000), this);
-			rotateBackward.setToAngle(0);
-			rotateBackward.play();	
-		}
+	public boolean isOpen() {
+		return getRotate() > 0;
+	}
+
+	public boolean isOfSameKind(Card otherCard) {
+		return this.faceView.getImage().equals(otherCard.faceView.getImage());
+	}
+
+	private void open(){
+		rotation = new RotateTransition(Duration.millis(1000), this);
+		rotation.setToAngle(90);
+		rotation.play();
+		rotation.setOnFinished((a) -> moveFaceToFront(() -> {} ));
+	}
+
+	private void moveFaceToFront(Runnable action) {
+		getChildren().clear();
+		getChildren().addAll(backView, faceView);
+		rotation.setToAngle(180);
+		rotation.play();
+		rotation.setOnFinished((a) -> { action.run(); close(); });
+	}
+
+	private void close() {
+		rotation.setToAngle(90);
+		rotation.play();
+		rotation.setOnFinished((a) -> moveFaceToBack(()->{}));
+	}
+
+	private void moveFaceToBack(Runnable action) {
+		getChildren().clear();
+		getChildren().addAll(faceView, backView);
+		rotation.setToAngle(0);
+		rotation.play();
+	}
 }
