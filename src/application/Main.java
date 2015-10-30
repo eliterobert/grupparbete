b1ba4ed32dde4a04ef28a331854e90a08e77af80
RobertE <roberteriksson_@hotmail.com>
2015-10-30 11:29:04 +0100

    changed startbutton, board is created when button is pressed

--- src/application/Main.java
+++ src/application/Main.java
@@ -27,12 +27,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {		
 		
 		startPage = new StartPage();
-		board = new Board();
 		startScene = new Scene(startPage, 1920*0.7, 1080*0.7);
-		boardScene = new Scene(board, 1920*0.7, 1080*0.7);
 		
 		startScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		boardScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
 		
 		primaryStage.setMinHeight(1080*0.73);
 		primaryStage.setMaxHeight(1080*0.73);
@@ -44,7 +42,14 @@ public class Main extends Application {
 		primaryStage.setScene(startScene);
 		primaryStage.show();
 		
-		startPage.startButton.setOnAction((a) -> primaryStage.setScene(boardScene));
+		startPage.startButton.setOnAction(event -> {
+			
+			board = new Board();
+			boardScene = new Scene(board, 1920 * 0.7, 1080 * 0.7);
+			boardScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(boardScene);
+			primaryStage.show();
+		});
 
 	}
 	
