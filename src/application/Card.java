5192efe8dc1df713572dd29ee3d29e3dfd014721
Bahawodin <bahawodin@gmail.com>
2015-11-02 19:21:25 +0100

    cleaned up Card class

--- src/application/Card.java
+++ src/application/Card.java
@@ -90,8 +90,6 @@ public class Card extends StackPane {
 	}
 
 	public boolean isOfSameKind(Card otherCard) {
-		// return
-		// this.faceView.getImage().equals(otherCard.faceView.getImage());
 
 		PixelReader faceImg = faceView.getImage().getPixelReader();
 		PixelReader otherFaceImg = otherCard.faceView.getImage().getPixelReader();
